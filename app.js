9663d89571f0943ebc68781c97f2dafedf5ac64c
Sukhmanjit Singh <singh.sukhmanjit@gmail.com>
2018-07-21 14:43:48 +0530

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.use("/", (req, res) => {
+ res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+ console.log("Server listening on port " + port);
+});
